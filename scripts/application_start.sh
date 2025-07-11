e81faa398ef809f4255faf14b398d3d366d811a9
root <root@ip-172-31-84-202.ec2.internal>
2025-07-11 07:21:51 +0000

    Fix: use pm2 start instead of restart

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,5 +2,6 @@
 
 echo 'run application_start.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 
-echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
-pm2 restart nodejs-express-app >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+echo 'pm2 start app.js --name nodejs-express-app' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+pm2 start /home/ec2-user/nodejs-aws-codedeploy-pipeline/app.js --name nodejs-express-app >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+
